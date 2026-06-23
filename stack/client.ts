34179857765d26099902f69533ef182692b2f82f
Francisyeremiah <francisyeremiah@gmail.com>
2026-06-23 21:02:52 +0300

    Update client.ts

--- stack/client.ts
+++ stack/client.ts
@@ -1,6 +1,6 @@
 import { HexclaveClientApp } from "@hexclave/next";
 
-const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
+const appUrl = process.env.NEXT_PUBLIC_APP_URL ?? "http://wiki-masters-new.vercel.app";
 
 export const hexclaveClientApp = new HexclaveClientApp({
   tokenStore: "nextjs-cookie",
